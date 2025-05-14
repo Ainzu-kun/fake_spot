31559c7ca10e14f072330fa6fff2ae608ddd98fb
rafidhp <rafiislamipasha27@gmail.com>
2025-05-14 16:08:33 +0700

    fix: fix if data empty

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,12 +10,12 @@ use App\Models\Semester;
 class AdminController extends Controller
 {
     public function welcome() {
-        $dosens = Dosen::limit(5)->get();
-        $matkuls = Matakuliah::orderBy('nama_mk')->limit(5)->get();
+        $dosens = Dosen::limit(5)->get() ?? collect();
+        $matkuls = Matakuliah::orderBy('nama_mk')->limit(5)->get() ?? collect();
         $semesters = Semester::orderBy('tahun_ajaran', 'desc')
                             ->orderBy('semester')
                             ->limit(5)
-                            ->get();
+                            ->get() ?? collect();
 
         return view('welcome', compact('dosens', 'matkuls', 'semesters'));
     }
